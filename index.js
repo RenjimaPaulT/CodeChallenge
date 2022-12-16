d4e693c21014028396b456492d36bfdfed13b161
RenjiT <renjis33@gmail.com>
2022-12-16 13:08:36 +1100

    lines changed

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 
-let totalamount =0;
+let totalamount = 0;
 
-const total1 = document.querySelector("#btn1");
-let input1 = document.querySelector('#item1')
-total1.addEventListener('click',displayTotal(input1.value));
+const total1 = document.getElementById("btn1");
+const input1 = document.getElementById('item1')
+console.log(input1.value)
+let a = input1.value ;
+//total1.addEventListener('click',displayTotal(input1.value));
+total1.addEventListener("click", function() {
+    displayTotal(a);
+  });
+  
 
 const total2 = document.querySelector("#btn2");
-let input2 = document.querySelector('#item2')
+const input2 = document.querySelector('#item2')
 total1.addEventListener('click',displayTotal(input2.value));
 
 const total3 = document.querySelector("#btn3");
@@ -15,7 +21,7 @@ total3.addEventListener('click',displayTotal(input3.value));
 
 function displayTotal(e)
 {
- totalamount= totalamount + e;
+ totalamount = totalamount + e;
 }
 let input4 =document.querySelector("#total");
 input4.value = totalamount;
